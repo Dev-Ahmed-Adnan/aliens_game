b7a5b1cf1c3f88271d6aaead930deb637ca3526d
aadnan1 <a.adnan@callhealth.care>
2026-06-21 02:59:29 +0300

    added movement and jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Debug.Log("Hello World" + Time.time);
+        float horizontal = 0f;
+        if (Keyboard.current != null)
+        {
+            if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed)
+                horizontal += 1f;
+            if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
+                horizontal -= 1f;
+        }
+
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+
+        var vertical = rb.linearVelocity.y;
+
+        if (Keyboard.current != null && Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            vertical = 5;
+        }
+
+        rb.linearVelocity = new Vector2(horizontal, vertical);
     }
 }
