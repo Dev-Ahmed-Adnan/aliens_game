f4f199c31dbacc26ff50c9a882880bd90b86b74f
aadnan1 <a.adnan@callhealth.care>
2026-06-24 20:55:40 +0300

    added 3d sound for the frog

--- Assets/Scripts/Frog.cs
+++ Assets/Scripts/Frog.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(AudioSource))]
 public class Frog : MonoBehaviour
 {
     [SerializeField] float _jumpDelay = 3;
@@ -9,11 +12,13 @@ public class Frog : MonoBehaviour
 
     private Rigidbody2D _rb;
     private SpriteRenderer _spriteRenderer;
+    private AudioSource _audioSource;
 
     void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
+        _audioSource = GetComponent<AudioSource>();
         _defaultSprite = _spriteRenderer.sprite;
         InvokeRepeating("Jump", _jumpDelay, _jumpDelay);
     }
@@ -21,6 +26,7 @@ public class Frog : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision)
     {
         _spriteRenderer.sprite = _defaultSprite;
+        if (collision.collider != null) _audioSource.Play();
     }
 
     void Jump()
